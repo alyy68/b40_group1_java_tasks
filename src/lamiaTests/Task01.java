e2d44b594014193c67529a4620d1e9edd314320a
lamiarahmanova <lamiya828@gmail.com>
2025-07-30 22:21:25 +0200

    added task 1 and 2

---
+++ src/lamiaTests/Task01.java
@@ -0,0 +1,8 @@
+package lamiaTests;
+
+public class Task01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Test line ");
+    }
+}
