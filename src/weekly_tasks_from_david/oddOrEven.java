b90ba9f9dd625afd55472555a56b10aad497a3b5
niloofarsarvari <n.sarvari97@gmail.com>
2025-08-05 21:30:58 +0200

    Task oddOrEven is done.

--- src/weekly_tasks_from_david/oddOrEven.java
+++ src/weekly_tasks_from_david/oddOrEven.java
@@ -2,8 +2,20 @@ package weekly_tasks_from_david;
 
 public class oddOrEven {
     public static void main(String[] args) {
-        //Write  a method which can identifies given number is even or odd
+        //Write a method which can identifies given number is even or odd
+        EvenOrOdd(198);
+        EvenOrOdd(171);
 
+
+
+    }
+
+    public static void EvenOrOdd (int a){
+        if(a% 2 == 0){
+            System.out.println(a + " is an even number");
+
+        }else
+            System.out.println(a + " is an odd number");
 
     }
 }
