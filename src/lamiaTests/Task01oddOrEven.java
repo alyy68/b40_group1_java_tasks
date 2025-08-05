06a647fe9a121ae367e14064c4d80c7043d4b78c
lamiarahmanova <lamiya828@gmail.com>
2025-08-05 21:57:11 +0200

    added task 1 Odd or even Number Lamia

---
+++ src/lamiaTests/Task01oddOrEven.java
@@ -0,0 +1,14 @@
+package lamiaTests;
+
+public class Task01oddOrEven {
+    public static void main(String[] args) {
+        // Write  a method which can identifies given number is even or odd
+         int a=11;
+         if (a%2==1){
+             System.out.println("a is an odd number ");
+         } else {
+             System.out.println("a is an Even number");
+         }
+
+    }
+}
