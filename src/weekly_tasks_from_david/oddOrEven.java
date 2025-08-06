ae7e00f45551bc6ca282b254065ca84381dc82c1
aliti <alitipirdamaz0@gmail.com>
2025-08-06 21:50:00 +0300

    Odd or Even task solved.

--- src/weekly_tasks_from_david/oddOrEven.java
+++ src/weekly_tasks_from_david/oddOrEven.java
@@ -3,16 +3,7 @@ package weekly_tasks_from_david;
 public class oddOrEven {
     public static void main(String[] args) {
         //Write a method which can identifies given number is even or odd
-        System.out.println(oddOrEven(8));
     }
 
-    public static String oddOrEven(int number) {
-        String result = "";
-        if (number % 2 == 0) {
-            result = "Even";
-        } else {
-            result = "Odd";
-        }
-        return result;
-    }
+
 }
