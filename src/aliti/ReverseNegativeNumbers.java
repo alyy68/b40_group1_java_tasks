eec68f4020cad1ebe525894c1e7a7c092ba28918
aliti <alitipirdamaz0@gmail.com>
2025-08-19 21:37:56 +0300

    Reverse negative numbers solved 2.0.

--- src/aliti/ReverseNegativeNumbers.java
+++ src/aliti/ReverseNegativeNumbers.java
@@ -6,12 +6,16 @@ public class ReverseNegativeNumbers {
         System.out.println(reverseNumbers(53));
         System.out.println(reverseNumbers(-53));
         System.out.println(reverseNumbers(0));
+        System.out.println(reverseNumbers(-3));
+        System.out.println(reverseNumbers(3));
     }
 
 
     public static int reverseNumbers(int number) {
 
-        if (number < 0) {
+        if (number < 0 && number > -10) {
+            return number;
+        } else if (number < 0) {
             int firstDigit = number % 10;
             number = number / 10;
             int result = firstDigit * 10 + number;
@@ -19,5 +23,6 @@ public class ReverseNegativeNumbers {
         } else {
             return number;
         }
+
     }
 }
