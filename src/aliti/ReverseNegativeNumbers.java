1c037b360bc49c7d46e62146e7ced293a5e1b92e
aliti <alitipirdamaz0@gmail.com>
2025-08-20 19:54:13 +0300

    Reverse negative numbers solved 3.0.

--- src/aliti/ReverseNegativeNumbers.java
+++ src/aliti/ReverseNegativeNumbers.java
@@ -3,14 +3,16 @@ package aliti;
 public class ReverseNegativeNumbers {
     public static void main(String[] args) {
 
-        System.out.println(reverseNumbers(53));
+        System.out.println(reverseNumbers(539));
+        System.out.println(reverseNumbers(-539));
         System.out.println(reverseNumbers(-53));
+        System.out.println(reverseNumbers(53));
         System.out.println(reverseNumbers(0));
         System.out.println(reverseNumbers(-3));
         System.out.println(reverseNumbers(3));
     }
 
-
+    /*
     public static int reverseNumbers(int number) {
 
         if (number < 0 && number > -10) {
@@ -24,5 +26,21 @@ public class ReverseNegativeNumbers {
             return number;
         }
 
+    }
+
+     */
+
+    public static int reverseNumbers(int number) {
+        int result = 0;
+        if (number < 0) {
+            while (number != 0) {
+                int digit = number % 10;
+                number /= 10;
+                result = result * 10 + digit;
+            }
+            return result;
+        } else {
+            return number;
+        }
     }
 }
