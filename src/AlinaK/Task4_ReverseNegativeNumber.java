3e94ac4958ad0486476e58a86bb9c7f5b636020e
AlinaKhrystiuk <sachuk.alna@gmail.com>
2025-08-22 22:04:46 +0300

    I changed my Task4

--- src/AlinaK/Task4_ReverseNegativeNumber.java
+++ src/AlinaK/Task4_ReverseNegativeNumber.java
@@ -2,12 +2,15 @@ package AlinaK;
 
 public class Task4_ReverseNegativeNumber {
     public static void main(String[] args) {
-        System.out.println(reversedNegativeNumber(-35));
+        System.out.println(reverseNumbers(-35));
+        System.out.println(reverseNumbers(-345));
+        System.out.println(reverseNumbers(-7));
+        System.out.println(reverseNumbers(-856));
 
     }
 
 
-    public static int  reversedNegativeNumber(int  num){
+    /*  public static int  reversedNegativeNumber(int  num){
         int result=0;
 
         if (num>0){
@@ -21,5 +24,20 @@ public class Task4_ReverseNegativeNumber {
             result=10*digit+num;
         }
         return result;
+    }*/
+
+
+    public static int reverseNumbers(int number) {
+        int result = 0;
+        if (number < 0) {
+            while (number != 0) {
+                int digit = number % 10;
+                number /= 10;
+                result = result * 10 + digit;
+            }
+            return result;
+        } else {
+            return number;
+        }
     }
 }
